2ee98ae4d53b1f586e748d679ec75115436815bf
artemPersik <artemstavropol2003@gmail.com>
2021-02-25 19:22:05 +0300

    2я задача

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def start():
-    return "http://127.0.0.1:8080/index/название"  # с заботой о вас))
+    return """<a href="http://127.0.0.1:8080/training/врач">врач</a> <br>
+                <a href="http://127.0.0.1:8080/training/инженер">инженер</a> <br>
+                <a href="http://127.0.0.1:8080/training/сантехник">сантехник</a> <br>
+                <a href="http://127.0.0.1:8080/training/бомж">бомж</a>"""  # с заботой о вас 2.0
 
 
-@app.route('/index/<title>')
-def index(title):
-    param = {"title": title}
-    return render_template('index.html', **param)
+@app.route('/training/<prof>')
+def index(prof):
+    profs = {"врач": url_for('static', filename='img/doctor.png'),
+             "инженер": url_for('static', filename='img/ingener.png'),
+             "сантехник": url_for('static', filename='img/santeh.png')}
+
+    if prof.lower() in profs:
+        print(profs[prof])
+        return render_template("index.html", schem=profs[prof])
+    else:
+        return render_template('index.html', schem=url_for('static', filename='img/error.png'))
 
 
 if __name__ == '__main__':
