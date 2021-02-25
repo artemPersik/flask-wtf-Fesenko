750386342ce360c66babbf3408658440205198b2
artemPersik <artemstavropol2003@gmail.com>
2021-02-25 18:30:22 +0300

    ?

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return "http://127.0.0.1:8080/index/название"
+
+
+@app.route('/index/<title>')
+def index(title):
+    param = {"title": title}
+    return render_template('index.html', **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
