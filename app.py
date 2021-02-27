b240689209bdc23bc33b295a0a9f9ca70ff2c2da
artemPersik <artemstavropol2003@gmail.com>
2021-02-27 14:43:19 +0300

    4Я ЗАДАЧА

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return """<a href="http://127.0.0.1:8080/list_prof/ol">ol</a> <br>
+    return """<a href="http://127.0.0.1:8080/auto_answer/анкета/Огурцов/Валера/высшее/Инженер/муж/хочу/да">в строке</a> <br>
                 <a href="http://127.0.0.1:8080/list_prof/ul">ul</a> <br>
                 <a href="http://127.0.0.1:8080/list_prof/бомж">бомж</a>"""  # с заботой о вас 2.0
 
 
-@app.route('/list_prof/<param>')
-def index(param):
-    if param == "ol" or param == "ul":
-        jobs = ["врач", "инженер", "сантехник", "бомж", "пилот", "биолог"]
-        return render_template("index.html", jobs=jobs, param=param)
-    else:
-        return f"""<img src="{url_for('static', filename='img/error.png')}">"""
+@app.route('/auto_answer/<title>/<surname>/<name>/<education>/<profession>/<sex>/<motivation>/<ready>')
+def index(title, surname, name, education, profession, sex, motivation, ready):
+    params = {"title": title,
+              "surname": surname,
+              "name": name,
+              "education": education,
+              "profession": profession,
+              "sex": sex,
+              "motivation": motivation,
+              "ready": ready}
+    return render_template('index.html', **params)
+
 
 
 if __name__ == '__main__':
